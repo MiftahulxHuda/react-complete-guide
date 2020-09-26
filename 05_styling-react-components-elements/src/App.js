a6f226104d92ff7f3ff92eaf5c536c08e72c5d74
Miftahul Huda <huda@tura.group>
2020-09-26 21:55:17 +0700

    Section 5 - 9. Styled Components & Dynamic Styles

--- 05_styling-react-components-elements/src/App.js
+++ 05_styling-react-components-elements/src/App.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import './App.css';
 import Person from './Person/Person';
 const StyledButton = styled.button`
-  background-color: green;
+  background-color: ${props => props.alt ? 'red': 'green'};
   color: white;
   font: inherit;
   border: 1px solid blue;
@@ -11,7 +11,7 @@ const StyledButton = styled.button`
   cursor: pointer;
 
   &:hover {
-    background-color: lightgreen;
+    background-color: ${props => props.alt ? 'salmon': 'lightgreen'};
     color: black;
   }
 `;
@@ -88,11 +88,11 @@ class App extends Component {
         </div>
       );
 
-      style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black'
-      };
+      // style.backgroundColor = 'red';
+      // style[':hover'] = {
+      //   backgroundColor: 'salmon',
+      //   color: 'black'
+      // };
     }
 
     const classes = [];
@@ -107,7 +107,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>
-        <StyledButton onClick={this.togglePersonsHandler}>Toggle Persons</StyledButton>
+        <StyledButton alt={this.state.showPersons} onClick={this.togglePersonsHandler}>
+          Toggle Persons
+        </StyledButton>
         {persons}
       </div>
     );
