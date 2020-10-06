196c43791113bd9d0b6c0bf529564c036606069b
Miftahul Huda <huda@tura.group>
2020-10-06 13:58:41 +0700

    Section 8 - 14. Calculating the Ingredient Sum Dynamically

--- 08_burger-builder-(basic-version)/src/containers/BurgerBuilder/BurgerBuilder.js
+++ 08_burger-builder-(basic-version)/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -10,10 +10,10 @@ class BurgerBuilder extends Component {
 
     state = {
         ingredients: {
-            salad: 1,
-            balcon: 1,
-            cheese: 2,
-            meat: 2
+            salad: 0,
+            balcon: 0,
+            cheese: 0,
+            meat: 0
         }
     }
 
