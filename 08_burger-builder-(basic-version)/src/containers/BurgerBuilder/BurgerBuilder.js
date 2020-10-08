4382f2cdf052e5de919dd7ee7152375dd68159e9
Miftahul Huda <huda@tura.group>
2020-10-08 10:05:36 +0700

    Section 8 - 20. Adding the Order Button

--- 08_burger-builder-(basic-version)/src/containers/BurgerBuilder/BurgerBuilder.js
+++ 08_burger-builder-(basic-version)/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -23,7 +23,19 @@ class BurgerBuilder extends Component {
             cheese: 0,
             meat: 0
         }, 
-        totalPrice: 4
+        totalPrice: 4,
+        purchasable: false
+    }
+
+    updatePurchase(ingredients) {
+        const sum = Object.keys(ingredients)
+            .map(igKey => {
+                return ingredients[igKey];
+            })
+            .reduce((sum, el) => {
+                return sum + el;
+            }, 0);
+        this.setState({purchasable: sum > 0});
     }
 
     addIngredientHandler = (type) => {
@@ -37,6 +49,7 @@ class BurgerBuilder extends Component {
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice + priceAddition;
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.updatePurchase(updatedIngredients);
     }
 
     removeIngredientHandler = (type) => {
@@ -53,6 +66,7 @@ class BurgerBuilder extends Component {
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice - priceAddition;
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+        this.updatePurchase(updatedIngredients);
     }
 
     render() {
@@ -70,6 +84,7 @@ class BurgerBuilder extends Component {
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disabledInfo}
+                    purchasable={this.state.purchasable}
                     price={this.state.totalPrice} />
             </Aux>
         );
