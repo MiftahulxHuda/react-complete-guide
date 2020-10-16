76fc0f1ab4da0f9bae7d686fb9aa0a816264264d
Miftahul Huda <huda@tura.group>
2020-10-16 22:55:41 +0700

    Section 12 - 2. Building the Checkout Container

---
+++ 12_adding-routing-to-burger-builder/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Layout from './hoc/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
+
+class App extends Component {
+  // state = {
+  //   show: true
+  // };
+
+  // componentDidMount() {
+  //   setTimeout(() => {
+  //     this.setState({show: false});
+  //   }, 5000);
+  // }
+  
+  render() {
+    return (
+      <div>
+        <Layout>
+          <BurgerBuilder />
+          <Checkout />
+        </Layout>
+      </div>
+    );
+  }
+}
+
+export default App;
