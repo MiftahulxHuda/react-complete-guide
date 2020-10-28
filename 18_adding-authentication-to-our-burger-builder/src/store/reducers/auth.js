eccb463ebeb2d552c58893d9913daaefdb696d2b
Miftahul Huda <huda@tura.group>
2020-10-29 04:47:42 +0700

    Section 18 - 9. Adding a Spinner

--- 18_adding-authentication-to-our-burger-builder/src/store/reducers/auth.js
+++ 18_adding-authentication-to-our-burger-builder/src/store/reducers/auth.js
@@ -35,7 +35,7 @@ const reducer = (state = initialState, action) => {
         case actionTypes.AUTH_SUCCESS: return authSuccess(state, action);
 
         case actionTypes.AUTH_FAIL: return authFail(state, action);
-    
+
         default: return state;
     }
 }
