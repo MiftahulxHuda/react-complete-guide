74275ef98a7c15259d91fed3b938b8c89d89e867
Miftahul Huda <huda@tura.group>
2020-10-28 19:57:51 +0700

    Section 18 - 8. Storing the Token

--- 18_adding-authentication-to-our-burger-builder/src/index.js
+++ 18_adding-authentication-to-our-burger-builder/src/index.js
@@ -10,6 +10,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
+import authReducer from './store/reducers/auth';
 
 const composeEnhancers =
   typeof window === 'object' &&
@@ -20,7 +21,8 @@ const composeEnhancers =
 
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
-  order: orderReducer
+  order: orderReducer,
+  auth: authReducer
 });
     
 const store = createStore(rootReducer, composeEnhancers(
