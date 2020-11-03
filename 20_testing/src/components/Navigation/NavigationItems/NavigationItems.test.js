b214a7a71066cefc5b8d292eb56a417d1dd319e1
Miftahul Huda <huda@tura.group>
2020-11-03 10:11:37 +0700

    Section 20 - 8. Testing Components Correctly

--- 20_testing/src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ 20_testing/src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -4,7 +4,7 @@ import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import NavigationItems from './NavigationItems';
-import navigationItem from './NavigationItem/NavigationItem';
+import NavigationItem from './NavigationItem/NavigationItem';
 
 configure({adapter: new Adapter()});
 
@@ -16,12 +16,17 @@ describe('<NavigationItems />', () => {
     });
     
     it('should render two <NavigationItem /> elements if not authenticated', () => {
-        expect(wrapper.find(navigationItem)).toHaveLength(2);
+        expect(wrapper.find(NavigationItem)).toHaveLength(2);
     });
 
     it('should render three <NavigationItem /> elements if authenticated', () => {
         // wrapper = shallow(<NavigationItems isAuthenticated />);
         wrapper.setProps({isAuthenticated: true});
-        expect(wrapper.find(navigationItem)).toHaveLength(3);
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
+    });
+
+    it('should an exact logout button', () => {
+        wrapper.setProps({isAuthenticated: true});
+        expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
     });
 });
